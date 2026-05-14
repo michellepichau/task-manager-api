0b3179b104251a7acd11468492788c41cde2d48a
Michelle Pichau <50593955+michellepichau@users.noreply.github.com>
2026-05-14 18:03:31 -0300

    Remove docstring from Task model
    Removed docstring from Task model class.

--- fastapi-project/app/models/task.py
+++ fastapi-project/app/models/task.py
@@ -5,10 +5,6 @@ from app.db.database import Base
 
 
 class Task(Base):
-    """
-    Model que representa a tabela 'tasks' no PostgreSQL.
-    Cada atributo = uma coluna na tabela.
-    """
     __tablename__ = "tasks"
 
     id          = Column(Integer, primary_key=True, index=True)
